0a2e6e0017dadd0d4ee1e7ef52a09ab12db12496
shanmugapriya16 <mshanmugapriya16@gmail.com>
2020-04-02 23:18:51 +0530

    Third program

---
+++ src/org/edu/Arts.java
@@ -0,0 +1,40 @@
+package org.edu;
+
+public class Arts extends Education{
+	public void bsc() {
+		// TODO Auto-generated method stub
+	System.out.println("course name bsc");
+	}
+
+	public void bEd() {
+		// TODO Auto-generated method stub
+	System.out.println("course name is BEd");
+	}
+
+	public void bcom() {
+		// TODO Auto-generated method stub
+	System.out.println("course name is bcom");
+	}
+
+	public void bba() {
+		// TODO Auto-generated method stub
+	System.out.println("course name is bba");
+	}
+	
+@Override
+public void ug() {
+	System.out.println("UG  courses changed");
+}
+	
+@Override
+public void pg() {
+	System.out.println("PG courses changed");
+}
+public static void main(String[] args) {
+	Arts a = new Arts();
+	a.bba();
+	a.bcom();
+	a.ug();
+	a.pg();
+}
+}
---
+++ src/org/edu/Education.java
@@ -0,0 +1,14 @@
+package org.edu;
+
+public class Education {
+
+	public void ug() {
+		// TODO Auto-generated method stub
+	System.out.println("UG courses");
+	}
+
+	public void pg() {
+		// TODO Auto-generated method stub
+	System.out.println("PG courses");
+	}
+}
